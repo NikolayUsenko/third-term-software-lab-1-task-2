a45f9a34c23c1c64f2f3123d7fbb3c34e7f6331e
Николай <usenkko.nikolay@gmail.com>
2025-10-11 14:25:36 +0300

    Добавьте файлы проекта.

---
+++ Software-Lab1-Task2/Cube.cs
@@ -0,0 +1,27 @@
+﻿class Cube
+{
+    private double side;
+    public Cube(double side) { this.side = side; }
+    public double GetV() => side * side * side;
+    public double GetS() => 6 * side * side;
+    public void PrintInfo()
+    {
+        String str =
+            $"           {side} \n" +
+            "     +-----------+\n" +
+            "    /           /|\n" +
+            "   /           / |\n" +
+            "  /           /  |\n" +
+            " +-----------+   |\n" +
+            " |           |   |\n" +
+            " |           |  /\n" +
+            " |           | /\n" +
+            " |           |/\n" +
+            " +-----------+\n";
+        Console.Write("\n" + str + "\n");
+        Console.WriteLine("Информация о кубе:");
+        Console.WriteLine($"Сторона: {side}");
+        Console.WriteLine($"Объем: {GetV()}");
+        Console.WriteLine($"Площадь поверхности: {GetS()}");
+    }
+}
---
+++ Software-Lab1-Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите длину стороны куба: ");
+        double side = Convert.ToDouble(Console.ReadLine());
+        while (side <= 0)
+        {
+            Console.Write("\nСторона куба должна быть положительной. Попробуйте снова: ");
+            side = Convert.ToDouble(Console.ReadLine());
+        }
+        Cube cube = new(side);
+#if DEBUG
+        Console.WriteLine($"\n[DEBUG] Создан объект класса Cube со стороной side = {side}");
+#else
+        cube.PrintInfo();
+#endif
+#if DEBUG
+        Console.WriteLine("\nПрограмма завершена (режим DEBUG).");
+#else
+        Console.WriteLine("\nПрограмма завершена (режим RELEASE).");
+#endif
+    }
+}
